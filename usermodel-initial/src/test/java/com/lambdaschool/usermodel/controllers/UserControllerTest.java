65e02d617d52d0e5e97c65c4456289b678d3c9b8
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-10-14 15:29:58 -0500

    userService has appropriate coverage

---
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerTest.java
@@ -0,0 +1,126 @@
+package com.lambdaschool.usermodel.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.usermodel.models.User;
+import com.lambdaschool.usermodel.services.UserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = UserController.class)
+public class UserControllerTest
+{
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private List<User> userList;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        userList = new ArrayList<>();
+
+        User u1 = new User("admin",
+                "password",
+                "admin@lambdaschool.local");
+        u1.setUserid(10);
+        userList.add(u1);
+        User u2 = new User("cinnamon",
+                "1234567",
+                "cinnamon@lambdaschool.local");
+        u2.setUserid(20);
+        userList.add(u2);
+        User u3 = new User("barnbarn",
+                "ILuvM4th!",
+                "barnbarn@lambdaschool.local");
+        u3.setUserid(30);
+        userList.add(u3);
+        User u4 = new User("puttat",
+                "password",
+                "puttat@school.lambda");
+        u4.setUserid(40);
+        userList.add(u4);
+        User u5 = new User("misskitty",
+                "password",
+                "misskitty@school.lambda");
+        u5.setUserid(50);
+        userList.add(u5);
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void listAllUsers() throws Exception
+    {
+        String apiUrl = "/users/users";
+        Mockito.when(userService.findAll()).thenReturn(userList);
+
+        RequestBuilder rb =
+                MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        String tr = r.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList);
+
+        assertEquals(er, tr);
+    }
+
+    @Test
+    public void getUserById()
+    {
+    }
+
+    @Test
+    public void getUserByName()
+    {
+    }
+
+    @Test
+    public void getUserLikeName()
+    {
+    }
+
+    @Test
+    public void addNewUser()
+    {
+    }
+
+    @Test
+    public void updateFullUser()
+    {
+    }
+
+    @Test
+    public void updateUser()
+    {
+    }
+
+    @Test
+    public void deleteUserById()
+    {
+    }
+}
